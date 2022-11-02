6c0cafb8790bf2e768006a1160e98f662a52b134
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-11-02 21:21:30 +0100

    add functionality for button, change quotes

--- app.js
+++ app.js
@@ -1,13 +1,46 @@
-const quoteOne = {
-  text: "I do the very best I know how - the very best I can: and I mean to keep on doing so until the end.",
-  author: "ABRAHAM LINCOLN",
-};
-const quoteTwo = {
-  text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius placeat sunt ex unde voluptatem odit voluptates ab! Modi explicabo consectetur provident similique assumenda inventore ipsam, adipisci voluptatem expedita sed nulla?",
-  author: "Random",
-};
-
-const quotes = [quoteOne, quoteTwo];
-
 const quoteText = document.querySelector(".text");
 const quoteAuthor = document.querySelector(".author");
+const btn = document.querySelector(".btn");
+
+const quotes = [
+  {
+    text: "I do the very best I know how - the very best I can: and I mean to keep on doing so until the end.",
+    author: "ABRAHAM LINCOLN",
+  },
+  {
+    text: "To, że milczę, nie znaczy, że nie mam nic do powiedzenia.",
+    author: "Jonathan Carroll",
+  },
+  {
+    text: "Życie jest wspaniałe, należy tylko spoglądać na nie przez właściwe okulary",
+    author: "Alexandre Dumas",
+  },
+  {
+    text: "Nieważne, skąd pochodzisz. Ważne, dokąd zmierzasz.",
+    author: "Brian Tracy",
+  },
+  {
+    text: "By wygrywać, muszę umieć podjąć decyzję w ułamku sekundy, a potem muszę umieć sobie poradzić z jej konsekwencjami.",
+    author: "Jelena Diemientiewa",
+  },
+  {
+    text: "Co skraca mi czas? – Działanie! Co wydłuża go niemiłosiernie? – Bezczynność!",
+    author: "Johann Wolfgang von Goethe",
+  },
+  {
+    text: "Nie przepracowałem ani jednego dnia w swoim życiu. Wszystko, co robiłem, to była przyjemność. ",
+    author: "Thomas Edison",
+  },
+  {
+    text: "W dwa miesiące możesz zdobyć więcej przyjaciół, interesując się innymi ludźmi, niż w ciągu dwóch lat, usiłując zainteresować sobą innych. ",
+    author: "Dale Carnegie",
+  },
+];
+
+function changeQuote() {
+  const random = Math.floor(Math.random() * quotes.length);
+  quoteText.innerText = quotes[random].text;
+  quoteAuthor.innerText = quotes[random].author;
+}
+
+btn.addEventListener("click", changeQuote);
