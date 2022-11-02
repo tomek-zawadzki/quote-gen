8f0953e28e5c50cf4cd3b1526dc5288175eed17f
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-11-02 10:21:30 +0100

    style for all html elements

---
+++ app.js
@@ -0,0 +1,13 @@
+const quoteOne = {
+  text: "I do the very best I know how - the very best I can: and I mean to keep on doing so until the end.",
+  author: "ABRAHAM LINCOLN",
+};
+const quoteTwo = {
+  text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius placeat sunt ex unde voluptatem odit voluptates ab! Modi explicabo consectetur provident similique assumenda inventore ipsam, adipisci voluptatem expedita sed nulla?",
+  author: "Random",
+};
+
+const quotes = [quoteOne, quoteTwo];
+
+const quoteText = document.querySelector(".text");
+const quoteAuthor = document.querySelector(".author");
